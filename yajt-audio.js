4bb345667b291b9e4f135e0fff6df92d2ca7b387
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-07-05 14:49:36 -0400

    spectrum seems to work

--- yajt-audio.js
+++ yajt-audio.js
@@ -49,17 +49,24 @@ YAJT.audio = {
             
         },
         spectrum: function(samples) {
+            var chunks = 16;
+            var sub_samples_a = new Float32Array(chunks);
+            var sub_samples_b = new Float32Array(chunks);
+            sub_samples_a[0] = 0;
+            sub_samples_b[0] = 0;
+            for (var i = 1; i < chunks; i++) {
+                var sub_sample_length = Math.floor(samples.length / chunks);
+                var start = (i-1) * sub_sample_length;
+                var end = i * sub_sample_length;
+                var sum = 0;
+                $.each(samples.slice(start, end), function(i, el) {sum += el;} );
+                sub_samples_a[i] = sum / sub_sample_length;
+                sub_samples_b[i] = 0;
+            }
+
             var audio_context = YAJT.audio.context;
             var oscillator = audio_context.createOscillator();
-
-            var real = new Float32Array(2);
-            var imag = new Float32Array(2);
-            real[0] = 0;
-            imag[0] = 0;
-            real[1] = 1;
-            imag[1] = 0;
-
-            var wave = audio_context.createPeriodicWave(real, imag);
+            var wave = audio_context.createPeriodicWave(sub_samples_a, sub_samples_b);
 
             oscillator.setPeriodicWave(wave);
             oscillator.frequency.value = Math.pow(2, YAJT.core.config.tone / 6) * 440;
