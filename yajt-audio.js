92967196e64cd2e28d76fa9253a48eed8a332ad8
Chuck McCallum <chuck_mccallum@wgbh.org>
2016-07-05 16:10:31 -0400

    Sequencer works

--- yajt-audio.js
+++ yajt-audio.js
@@ -17,14 +17,14 @@ YAJT.audio = {
                 return;
             }
         }
-        
+
         if (YAJT.audio.old_source) {
             YAJT.audio.old_source.stop(YAJT.audio.context.currentTime);
         }
         YAJT.audio.generate_from[YAJT.core.config.mode](samples);
     },
     generate_from: {
-        waveform: function(samples) {
+        waveform: function (samples) {
             var audio_context = YAJT.audio.context;
             var channels = 1;
             var frame_count = samples.length;
@@ -45,21 +45,42 @@ YAJT.audio = {
             source.start(audio_context.currentTime);
             YAJT.audio.old_source = source;
         },
-        sequence: function(samples) {
-            
+        sequence: function (samples) {
+            var audio_context = YAJT.audio.context;
+            var count = 16;
+            var sub_sample_length = Math.floor(samples.length / count);
+
+            var index = Math.floor(audio_context.currentTime / (YAJT.core.config.timeout / 1000)) % count;
+            var start = index * sub_sample_length;
+            var end = (index + 1) * sub_sample_length;
+            var sum = 0;
+            $.each(samples.slice(start, end), function (i, el) {
+                sum += el;
+            });
+            var tone = Math.floor(12 * (1 - (sum / sub_sample_length / YAJT.core.config.height)));
+
+            var oscillator = audio_context.createOscillator();
+            oscillator.type = 'sine';
+            oscillator.frequency.value = Math.pow(2, (tone + YAJT.core.config.tone - 12) / 6) * 440;
+            oscillator.connect(audio_context.destination);
+            oscillator.start();
+
+            YAJT.audio.old_source = oscillator;
         },
-        spectrum: function(samples) {
-            var chunks = 16;
-            var sub_samples_a = new Float32Array(chunks);
-            var sub_samples_b = new Float32Array(chunks);
+        spectrum: function (samples) {
+            var count = 16;
+            var sub_samples_a = new Float32Array(count);
+            var sub_samples_b = new Float32Array(count);
             sub_samples_a[0] = 0;
             sub_samples_b[0] = 0;
-            for (var i = 1; i < chunks; i++) {
-                var sub_sample_length = Math.floor(samples.length / chunks);
-                var start = (i-1) * sub_sample_length;
+            var sub_sample_length = Math.floor(samples.length / count);
+            for (var i = 1; i < count; i++) {
+                var start = (i - 1) * sub_sample_length;
                 var end = i * sub_sample_length;
                 var sum = 0;
-                $.each(samples.slice(start, end), function(i, el) {sum += el;} );
+                $.each(samples.slice(start, end), function (i, el) {
+                    sum += el;
+                });
                 sub_samples_a[i] = sum / sub_sample_length;
                 sub_samples_b[i] = 0;
             }
@@ -72,7 +93,7 @@ YAJT.audio = {
             oscillator.frequency.value = Math.pow(2, YAJT.core.config.tone / 6) * 440;
             oscillator.connect(audio_context.destination);
             oscillator.start(audio_context.currentTime);
-            
+
             YAJT.audio.old_source = oscillator;
         }
     }
